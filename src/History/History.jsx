2692b51e77d8da020ead42d655bba8d4fa851012
Jcamilo8aR <camilo8ar23@gmail.com>
2023-04-18 19:22:06 -0500

    Comienzo Proyecto

---
+++ src/History/History.jsx
@@ -0,0 +1,31 @@
+import './History.css'
+
+export function History(){
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+
+                    <div className="col-12 col-md-4">
+                        <h2>Kendrick Lamar</h2>
+                        <p>
+                        Es un rapero, compositor y productor discográfico estadounidense. Ha sido aclamado por críticos, músicos y fanáticos por sus vívidas letras y narraciones sobre sus experiencias y problemáticas dentro de la cultura afroamericana y del hip-hop, llegando a ser nombrado "la voz líder de una generación
+                        </p>
+
+                        <img src="https://firebasestorage.googleapis.com/v0/b/musicosjcor.appspot.com/o/Damn.jpg?alt=media&token=87df942d-4026-48a2-aedd-6c7a5b72de53" alt="foto" className='img-fluid w-100' />
+                    </div>
+
+                    <div className="col-12 col-md-8">
+                        <p><br />
+                        Criado en Compton, California, Kendrick inició su carrera como un adolescente, bajo el seudónimo K-Dot. Tras lanzar un mixtape que llamó la atención local y que le brindó su primer contrato con la discográfica independiente,Top Dawg Entertainment, Lamar lanzó los proyectos Overly Dedicated (2010) y Section.80 (2011) que le brindaron un masivo seguimiento en Internet y la oportunidad de colaborar con varios artistas importantes de la escena del hip hop.
+                        </p>
+
+                        <img src="https://firebasestorage.googleapis.com/v0/b/musicosjcor.appspot.com/o/AlbumKL.webp?alt=media&token=497edc46-3cd8-440b-91b9-9b42535f7309" alt="foto" className='img-fluid w-100' />
+                    </div>
+
+                    
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/Members/Members.jsx
@@ -0,0 +1,7 @@
+export function Members(){
+    return(
+        <>
+        
+        </>
+    )
+}
